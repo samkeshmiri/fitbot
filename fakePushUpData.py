a516f4615cb56cfe8fc3eb3814cda22f3486c167
Deanlicious <evydw@nottingham.ac.uk>
2018-02-17 16:25:40 +0000

    added face detection, changed fake data to ossicalate 3 times

--- fakePushUpData.py
+++ fakePushUpData.py
@@ -2,4 +2,4 @@ from random import randint
 import math
 
 def fakeData():
-    return [math.sin(math.radians(x)) for x in range(0,360,5)]
+    return [math.sin(math.radians(x)) for x in range(0,1000,5)]
