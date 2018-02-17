faee744d5009a66709aa60bdd6e5c0080788b11e
SamLaptop <sam@github.com>
2018-02-17 20:23:23 +0000

    need to figure out wave frequency

--- DeanSamHeadData.py
+++ DeanSamHeadData.py
@@ -1,4 +1,12 @@
+
+from __future__ import division
+from numpy.fft import rfft
+from numpy import argmax, mean, diff, log
+from matplotlib.mlab import find
+from time import time
 import matplotlib.pyplot as plt
+import sys
+
 MainLit =[]
 with open('data1.txt') as f:
     for line in f:
@@ -6,9 +14,11 @@ with open('data1.txt') as f:
         MainLit.append(float((numbers_float[0])))
 
 print(MainLit)
-y = MainLit
-x = [i for i in range(len(y))]
+x = MainLit
+y = [i for i in range(len(y))]
 
 plt.plot(x,y)
 plt.show()
 
+f = x/y
+print(f)
